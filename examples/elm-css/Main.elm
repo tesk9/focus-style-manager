1028c9ef0514c68d1fda34d75b20817937659c25
Tessa Kelly <tessakkelly@gmail.com>
2018-02-16 15:43:25 -0800

    Adds an example use of the elm-css library

---
+++ examples/elm-css/Main.elm
@@ -0,0 +1,96 @@
+module Main exposing (..)
+
+import Css
+import Css.Colors
+import Css.Foreign
+import FocusStyleManager
+import Html
+import Html.Attributes
+import Html.Styled
+
+
+{-| Compile using `elm-make Main.elm --output index.html`
+-}
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { focusStyleManager : FocusStyleManager.Model
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { focusStyleManager = FocusStyleManager.keyboardUser
+      }
+    , Cmd.none
+    )
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div []
+        [ Html.fieldset []
+            [ Html.legend [] [ Html.text "Try clicking through these options. Then try tabbing/arrowing!" ]
+            , viewInput "a"
+            , viewInput "b"
+            , viewInput "c"
+            , viewInput "d"
+            ]
+        , FocusStyleManager.customStyle
+            { styleToTag =
+                \relevantStyle ->
+                    Css.Foreign.global
+                        [ Css.Foreign.everything [ Css.focus [ relevantStyle ] ]
+                        ]
+                        |> Html.Styled.toUnstyled
+            , keyboardUser = Css.outline3 (Css.px 3) Css.solid Css.Colors.fuchsia
+            , mouseUser = Css.outline Css.none
+            , touchUser = Css.outline Css.none
+            }
+            model.focusStyleManager
+        ]
+
+
+viewInput : String -> Html.Html msg
+viewInput description =
+    Html.label []
+        [ Html.input
+            [ Html.Attributes.type_ "radio"
+            , Html.Attributes.name "alphabet"
+            , Html.Attributes.value description
+            , Html.Attributes.id description
+            ]
+            []
+        , Html.text description
+        ]
+
+
+type Msg
+    = FocusStyleManagerMsg FocusStyleManager.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        FocusStyleManagerMsg focusStyleManagerMsg ->
+            let
+                focusStyleManager =
+                    FocusStyleManager.update focusStyleManagerMsg model.focusStyleManager
+            in
+            ( { model | focusStyleManager = focusStyleManager }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.map FocusStyleManagerMsg
+        (FocusStyleManager.subscriptions model.focusStyleManager)
